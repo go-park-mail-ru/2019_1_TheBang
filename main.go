0691aa399de6adf99b2b490b53736e7436da1610
a.baronskiy <a.baronskiy@mail.ru>
2019-03-13 14:48:47 +0300

    CORS для сервера

--- main.go
+++ main.go
@@ -54,9 +54,11 @@ func main() {
 func commonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
 		next.ServeHTTP(w, r)
-
-
 	})
 }
 
