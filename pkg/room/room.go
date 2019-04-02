578d8da03eb3399faafbd1bd5efcec163b82a4d5
a.baronskiy <a.baronskiy@mail.ru>
2019-04-02 13:34:17 +0300

    Коммит перед отдельной веткой room

--- pkg/room/room.go
+++ pkg/room/room.go
@@ -1,1 +1,19 @@
 package room
+
+type Room struct {
+	Name string `json:"name"`
+}
+
+var Rooms = map[string]Room{
+	"test": Room{
+		Name: "test",
+	},
+}
+
+func RoomsList() []Room {
+	list := []Room{}
+
+	for room := range Rooms {
+		append
+	}
+}
