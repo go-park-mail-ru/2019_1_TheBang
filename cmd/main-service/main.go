f10acc33fbb3c93bafbf03370261d8c5319c3179
a.baronskiy <a.baronskiy@mail.ru>
2019-04-17 23:49:55 +0300

    Коммит перед переездом рефакторингом и вынесенрием всех микросервисов в 1 репозиторий

---
+++ cmd/main-service/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"2019_1_TheBang/config"
+	"2019_1_TheBang/pkg/leaderboard"
+	"2019_1_TheBang/pkg/login"
+	"2019_1_TheBang/pkg/logout"
+	"2019_1_TheBang/pkg/middleware"
+	"2019_1_TheBang/pkg/user"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	defer config.Logger.Sync()
+	config.Logger.Info(fmt.Sprintf("FrontenDest: %v", config.FrontentDst))
+	config.Logger.Info(fmt.Sprintf("PORT: %v", config.PORT))
+
+	err := config.DB.Ping()
+	if err != nil {
+		config.Logger.Fatal("Can not start connection with database")
+	}
+
+	r := mux.NewRouter()
+	r.Use(middleware.AccessLogMiddleware,
+		middleware.CommonMiddleware,
+		middleware.AuthMiddleware)
+
+	r.HandleFunc("/auth", login.LogInHandler).Methods("POST")
+	r.HandleFunc("/auth", logout.LogoutHandler).Methods("DELETE", "OPTIONS")
+
+	r.HandleFunc("/leaderbord/{page:[0-9]+}", leaderboard.LeaderbordHandler).Methods("GET")
+
+	r.HandleFunc("/user", user.MyProfileCreateHandler).Methods("POST")
+	r.HandleFunc("/user", user.MyProfileInfoHandler).Methods("GET")
+	r.HandleFunc("/user", user.MyProfileInfoUpdateHandler).Methods("PUT", "OPTIONS")
+
+	r.HandleFunc("/user/avatar", user.ChangeProfileAvatarHandler).Methods("POST", "OPTIONS")
+
+	r.HandleFunc("/icon/{filename}", user.GetIconHandler).Methods("GET")
+
+	config.Logger.Fatal(http.ListenAndServe(":"+config.PORT, r))
+}
