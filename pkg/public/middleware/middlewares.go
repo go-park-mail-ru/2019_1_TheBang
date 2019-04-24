1a39519409a449c27585190e6158ace44b1a5082
a.baronskiy <a.baronskiy@mail.ru>
2019-04-24 13:15:16 +0300

    переименовал фаил мидллваров

--- pkg/public/middleware/middlewares.go
+++ pkg/public/middleware/middlewares.go
@@ -1,11 +1,10 @@
 package middleware
 
 import (
-	"net/http"
-	"time"
-
 	"2019_1_TheBang/config"
 	"2019_1_TheBang/pkg/public/auth"
+
+	"github.com/gin-gonic/gin"
 )
 
 type urlMehtod struct {
@@ -19,47 +18,27 @@ var ignorCheckAuth = map[urlMehtod]bool{
 	urlMehtod{URL: "/room", Method: "POST"}: true,
 }
 
-func AuthMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		check := urlMehtod{URL: r.URL.Path, Method: r.Method}
-		if ok := ignorCheckAuth[check]; !ok {
-			if r.Method == "OPTIONS" {
-				return
-			}
+func CorsMiddlewareGin(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.Header("Access-Control-Allow-Origin", config.FrontentDst)
+	c.Header("Access-Control-Allow-Credentials", "true")
+	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	c.Header("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
-			if _, ok := auth.CheckTocken(r); !ok {
-				w.WriteHeader(http.StatusUnauthorized)
+	c.Next()
+}
 
-				return
-			}
+func AuthMiddlewareGin(c *gin.Context) {
+	if c.Request.Method == "OPTIONS" {
+		return
+	}
+
+	check := urlMehtod{URL: c.Request.URL.Path, Method: c.Request.Method}
+	if ok := ignorCheckAuth[check]; !ok {
+		if _, ok := auth.CheckTocken(c.Request); !ok {
+			return
 		}
+	}
 
-		next.ServeHTTP(w, r)
-	})
-}
-
-func AccessLogMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		next.ServeHTTP(w, r)
-
-		config.Logger.Infow("AppLog",
-			"method=", r.Method,
-			"path=", r.URL.Path,
-			"remAddr=", r.RemoteAddr,
-			"service=", time.Since(start))
-	})
-}
-
-func CommonMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", config.FrontentDst)
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-
-		next.ServeHTTP(w, r)
-	})
+	c.Next()
 }
