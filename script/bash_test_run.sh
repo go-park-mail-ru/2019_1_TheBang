d722423db53d1ca176ca075e631cf887463a4f89
a.baronskiy <a.baronskiy@mail.ru>
2019-04-29 18:56:49 +0300

    обновил скрипт запуска

--- script/bash_test_run.sh
+++ script/bash_test_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 go test -coverprofile=out.cover -coverpkg=$(go list ./...  | grep -v "test" |   tr '\n' ',') ./test/...
-go tool cover -func=out.cover 
+go tool cover -func=out.cover
+go tool cover -html=out.cover
--- script/run_all_services.sh
+++ script/run_all_services.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 go run cmd/auth-service/*.go . &
-go run cmd/game-service/*.go . &
 go run cmd/main-service/*.go . &
+go run cmd/chat-service/*.go . &
+go run cmd/game-service/*.go . &
