110cbcbbc984a1e653825fac9885113760b5a503
a.baronskiy <a.baronskiy@mail.ru>
2019-04-30 17:12:00 +0300

    50% тетсов

--- test/auth-service-test/authchecker_test.go
+++ test/auth-service-test/authchecker_test.go
@@ -1,14 +1,54 @@
 package test
 
-// import (
-// 	"testing"
-// 	"2019_1_TheBang/pkg/auth-service-pkg/authchecker"
-// 	pb "2019_1_TheBang/pkg/public/protobuf"
-// 	"google.golang.org/grpc"
-// )
-// func TestAuthcheckerServer(t *testing.T) {
-// 	s := grpc.NewServer()
-// 	pb.RegisterCookieCheckerServer(s, &authchecker.Server{})
+import (
+	"2019_1_TheBang/pkg/auth-service-pkg/authchecker"
+	pb "2019_1_TheBang/pkg/public/protobuf"
+	"context"
+	_ "google.golang.org/grpc"
+	"testing"
+)
 
+func TestAuthcheckerServerSUCCESS(t *testing.T) {
+	req := &pb.CookieRequest{
+		JwtToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+			"eyJpZCI6MSwibmlja25hbWUiOiIxMiIsInBob3RvX3VybCI6IjNkMDEzMmI" +
+			"4NTRmYmRlYWNmYzUwMmYyZmNjMWIyMjg2IiwiaXNzIjoiVGhlQmFuZyBzZXJ2ZX" +
+			"IifQ.ghh_8keORscXMbHatzjTn2o8bPN0IcWdPgcAReSV7sA",
+	}
 
-// }
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	s := authchecker.Server{}
+	res, err := s.CheckCookie(ctx, req)
+	if err != nil {
+		t.Errorf("TestAuthcheckerServerSUCCESS: %v", err.Error())
+	}
+
+	if !res.Valid {
+		t.Error("TestAuthcheckerServerSUCCESS, valid user is invalid")
+	}
+
+	user := res.GetUser()
+	if user.Nickname != "12" {
+		t.Error("TestAuthcheckerServerSUCCESS, incorrect nickname")
+	}
+}
+
+func TestAuthcheckerServerFAIL(t *testing.T) {
+	req := &pb.CookieRequest{
+		JwtToken: "invalid_token",
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	s := authchecker.Server{}
+	res, err := s.CheckCookie(ctx, req)
+	if err != nil {
+		t.Errorf("TestAuthcheckerServerSUCCESS: %v", err.Error())
+	}
+
+	if res.Valid {
+		t.Error("TestAuthcheckerServerSUCCESS, valid user is invalid")
+	}
+}
