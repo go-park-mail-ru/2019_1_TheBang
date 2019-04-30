fa1d10c8cc0398b33f25a2987bc3b731241f493a
a.baronskiy <a.baronskiy@mail.ru>
2019-04-30 20:36:09 +0300

    вынес вывод stderr в отдельный фаил

--- script/run_all_services.sh
+++ script/run_all_services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-go run cmd/auth-service/*.go . &
-go run cmd/main-service/*.go . &
-go run cmd/chat-service/*.go . &
-go run cmd/game-service/*.go . &
+go run cmd/auth-service/*.go . 2> logs/auth &
+go run cmd/main-service/*.go . 2> logs/main &
+go run cmd/chat-service/*.go . 2> logs/chat &
+go run cmd/game-service/*.go . 2> logs/game &
