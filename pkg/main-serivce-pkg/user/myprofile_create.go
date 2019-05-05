cbbde4ad3ffbaa1d1c455d7613c28e6dc7dc3c5f
a.baronskiy <a.baronskiy@mail.ru>
2019-05-05 16:56:29 +0300

    вход во время регистрации

--- pkg/main-serivce-pkg/user/myprofile_create.go
+++ pkg/main-serivce-pkg/user/myprofile_create.go
@@ -2,9 +2,14 @@ package user
 
 import (
 	"net/http"
+	"time"
 
 	"2019_1_TheBang/api"
+	"2019_1_TheBang/config"
+	"2019_1_TheBang/config/mainconfig"
+	"2019_1_TheBang/pkg/public/auth"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
@@ -26,5 +31,59 @@ func MyProfileCreateHandler(c *gin.Context) {
 		return
 	}
 
-	c.Status(http.StatusCreated)
+	ss, status := LoginAcount(signup.Nickname, signup.Passwd)
+	if status != http.StatusOK {
+		c.AbortWithStatus(status)
+
+		return
+	}
+
+	expiration := time.Now().Add(10 * time.Hour)
+	cookie := http.Cookie{
+		Name:     config.CookieName,
+		Value:    ss,
+		Expires:  expiration,
+		HttpOnly: true,
+		Path:     "",
+	}
+
+	http.SetCookie(c.Writer, &cookie)
+
+	prof, status := SelectUser(signup.Nickname)
+	if status != http.StatusOK {
+		c.AbortWithStatus(status)
+
+		return
+	}
+
+	c.JSONP(http.StatusCreated, prof)
+}
+
+func LoginAcount(username, passwd string) (ss string, status int) {
+	ok := CheckUser(username, passwd)
+	if !ok {
+		return ss, http.StatusUnauthorized
+	}
+
+	prof, _ := SelectUser(username)
+
+	claims := auth.CustomClaims{
+		prof.Id,
+		prof.Nickname,
+		prof.Photo,
+		jwt.StandardClaims{
+			Issuer: mainconfig.ServerName,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(config.SECRET)
+	if err != nil {
+		config.Logger.Warnw("LoginAcount",
+			"Error with JWT tocken generation:", err.Error())
+
+		return ss, http.StatusInternalServerError
+	}
+
+	return ss, http.StatusOK
 }
