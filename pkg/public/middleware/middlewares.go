220e4b4a314189ea4d232e1b047ccdfbc2306a43
a.baronskiy <a.baronskiy@mail.ru>
2019-05-29 19:26:59 +0300

    убрал корс

--- pkg/public/middleware/middlewares.go
+++ pkg/public/middleware/middlewares.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"2019_1_TheBang/pkg/public/auth"
-	"2019_1_TheBang/config"
 	"net/http"
 	"regexp"
 
@@ -25,15 +24,6 @@ var ignorCheckAuth = map[urlMehtod]bool{
 
 func CorsMiddlewareGin(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
-	c.Header("Access-Control-Allow-Origin", config.FrontentDst)
-	c.Header("Access-Control-Allow-Credentials", "true")
-	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	c.Header("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-
-	if c.Request.Method == "OPTIONS" {
-		c.AbortWithStatus(http.StatusNoContent)
-		return
-	}
 
 	c.Next()
 }
